2673b1b065ff941e3f2c734e2f1bdb98f277ab60
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2024-06-06 11:02:18 -0300

    Primeiro commit

---
+++ src/main/java/com/github/leandrokhalel/parking/Vehicle.java
@@ -0,0 +1,14 @@
+package com.github.leandrokhalel.parking;
+
+import java.util.UUID;
+
+public class Vehicle {
+
+    private UUID id;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private String tipo;
+
+}
